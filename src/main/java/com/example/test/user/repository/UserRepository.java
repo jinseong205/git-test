56f5df68b8ee39076edd9847c5166dbb7ec1a95d
dudwns9280 <chlqus9984@gmail.com>
2023-09-01 08:56:05 +0900

    feat user 임시 커밋

--- src/main/java/com/example/test/user/repository/UserRepository.java
+++ src/main/java/com/example/test/user/repository/UserRepository.java
@@ -1,5 +1,8 @@
 package com.example.test.user.repository;
 
-public interface UserRepository{
+import com.example.test.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
 
 }
