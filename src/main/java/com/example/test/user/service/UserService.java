611126bac9fbd056123a9c48b883766b3a6adbb9
dudwns9280 <chlqus9984@gmail.com>
2023-09-01 09:13:37 +0900

    feat : Read User 어쩌구저쩌구

--- src/main/java/com/example/test/user/service/UserService.java
+++ src/main/java/com/example/test/user/service/UserService.java
@@ -1,4 +1,16 @@
 package com.example.test.user.service;
 
+import com.example.test.user.entity.User;
+import com.example.test.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+    private final UserRepository userRepository;
+
+    public User readUser(Long id){
+        return userRepository.findById(id).orElseThrow();
+    }
 }
